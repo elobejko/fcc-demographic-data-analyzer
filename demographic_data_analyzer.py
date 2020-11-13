1acec1f68932464598bb51dbf412e29050a2b13a
Ewa Łobejko <lobejkoewa@gmail.com>
2020-11-13 17:36:52 +0000

    reading csv file

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
-
+    df = pd.read_csv('adult.data.csv')
+    #print(df.dtypes)
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = pd.Series(df.set_index('race', inplace = True))
+   # print(race_count.head())
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = None #df.mean[df['sex'] == 'Male']
+    #print(average_age_men)
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = None
